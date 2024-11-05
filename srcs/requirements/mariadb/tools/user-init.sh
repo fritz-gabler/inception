ed1886ead8dcc56e06dbbc715180a5b51e5cc8ba
fritz-gabler <fritzlmgaebler@gmail.com>
2024-11-05 20:32:16 +0100

    add starting of mariadb in init file

--- srcs/requirements/mariadb/tools/user-init.sh
+++ srcs/requirements/mariadb/tools/user-init.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-if [ -z "$MARIADB-DATABASE" ]; then
-  echo "MARIADB-DATABASE is not set"
+if [ -z "$MARIADB_DATABASE" ]; then
+  echo "MARIADB_DATABASE is not set"
   exit 1
 elif [ -z "$MARIADB_USER" ]; then
-  echo "MARIADB-DATABASE is not set"
+  echo "MARIADB_USER is not set"
   exit 1
 elif [ -z "$MARIADB_PASSWORD" ]; then
   echo "MARIADB_PASSWORD is not set"
   exit 1
 fi
 
-mariadb -p $MARIADB_PASSWORD
-CREATE USER IF NOT EXISTS $MARIADB_USER IDENTIFIED BY $MARIADB_PASSWORD;
-CREATE DATABASE $$MARIADB-DATABASE;
-GRANT ALL PRIVILEGES ON *.* TO $MARIADB_USER@localhost IDENTIFIED BY $MARIADB_PASSWORD;
+
+if [ ! -f "init.sql" ]; then
+  touch /tools/init.sql
+  echo creaded file init.sql
+fi
+
+chmod 0777 /tools/init.sql
+cat << EOF > /tools/init.sql
+CREATE USER IF NOT EXISTS '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';
+CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;
+GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';
+EOF
+
+FILE=run/mysqld/mysqld.sock
+if [ ! -f "$FILE" ]; then
+  mkdir /run/mysqld/
+  touch /run/mysqld/mysqld.sock
+  echo created file mysqld.sock
+fi
+
+mariadbd --user=root --init-file=/tools/init.sql
