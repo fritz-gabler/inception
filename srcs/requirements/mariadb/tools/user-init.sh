aae958b5e60697a099976e9eccebe10ae5c07f60
fritz-gabler <fritzlmgaebler@gmail.com>
2024-11-15 21:14:40 +0100

    fix syntax in unser init file
    filling in the envs was done wrong

--- srcs/requirements/mariadb/tools/user-init.sh
+++ srcs/requirements/mariadb/tools/user-init.sh
@@ -19,16 +19,19 @@ fi
 
 chmod 0777 /tools/init.sql
 cat << EOF > /tools/init.sql
-CREATE USER IF NOT EXISTS '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';
-CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;
-GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
+GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
 EOF
 
 FILE=run/mysqld/mysqld.sock
+
 if [ ! -f "$FILE" ]; then
-  mkdir /run/mysqld/
+  mkdir -p /run/mysqld/
   touch /run/mysqld/mysqld.sock
   echo created file mysqld.sock
 fi
+
+sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 
 mariadbd --user=root --init-file=/tools/init.sql
