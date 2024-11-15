54cacaf9f95921e1f2430c97ef260af6bca221a9
fritz-gabler <fritzlmgaebler@gmail.com>
2024-11-15 21:18:39 +0100

    change structure in wordpress sh tool file

--- srcs/requirements/wordpress/tools/dowload_wp_cli.sh
+++ srcs/requirements/wordpress/tools/dowload_wp_cli.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root
+mkdir -p /var/www/html/mysite
 
-wp config create --dbname={$MARIADB_DATABASE} \
-  --dbuser={$MARIADB_USER} --dbpass={$MARIADB_PASSWORD} --allow-root
+if [ ! -f "/var/www/html/mysite/index.php" ]; then
+  wp core download --allow-root --path="/var/www/html/mysite/"
+fi
 
-wp core install --url=fgabler.42.fr --title=inception \
-   --admin_user={$WP_DB_USER} --admin_password={$WP_DB_USER_PASS} \
-   --admin_email={$WP_DB_EMAIL} --allow-root
+cd /var/www/html/mysite/
+
+wp config create \
+  --dbname=$MARIADB_DATABASE \
+  --dbuser=$MARIADB_USER \
+  --dbpass=$MARIADB_PASSWORD \
+  --dbhost='mariadb' \
+  --allow-root \
+  --path="/var/www/html/mysite/"
+
+wp core install \
+  --url=fgabler.42.fr \
+  --title=inception \
+  --admin_user=$WP_DB_USER \
+  --admin_password=$WP_DB_USER_PASS \
+  --admin_email=$WP_DB_EMAIL \
+  --allow-root
 
 tail -f
