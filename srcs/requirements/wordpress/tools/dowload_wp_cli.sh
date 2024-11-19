ff0e6d44a7a0b8219a92daaac9dbe9f698be71fd
fritz-gabler <fritzlmgaebler@gmail.com>
2024-11-19 19:17:57 +0100

    add if check in wordpres sh file
    beforee database was build even when it already existed

--- srcs/requirements/wordpress/tools/dowload_wp_cli.sh
+++ srcs/requirements/wordpress/tools/dowload_wp_cli.sh
@@ -12,6 +12,7 @@ fi
 
 cd /var/www/html/mysite/
 
+if [ ! -f "/var/www/html/mysite/wp-config.php" ]; then
 wp config create \
   --dbname=$MARIADB_DATABASE \
   --dbuser=$MARIADB_USER \
@@ -19,13 +20,18 @@ wp config create \
   --dbhost='mariadb' \
   --allow-root \
   --path="/var/www/html/mysite/"
+fi
 
-wp core install \
-  --url=fgabler.42.fr \
-  --title=inception \
-  --admin_user=$WP_DB_USER \
-  --admin_password=$WP_DB_USER_PASS \
-  --admin_email=$WP_DB_EMAIL \
-  --allow-root
+if ! wp core is-installed --path="/var/www/html/mysite/" --allow-root; then
+  wp core install \
+    --url=https://fgabler.42.fr \
+    --title=inception \
+    --admin_user=$WP_DB_USER \
+    --admin_password=$WP_DB_USER_PASS \
+    --admin_email=$WP_DB_EMAIL \
+    --allow-root
+fi
 
-tail -f
+php-fpm8.2 -F
+
+#tail -f
