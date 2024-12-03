642532e1fcea808af65f0c447de24d9c4fe238ec
fritz-gabler <fritzlmgaebler@gmail.com>
2024-12-03 20:18:48 +0100

    change creation of mysqld dir and permission setting

--- srcs/requirements/mariadb/tools/user-init.sh
+++ srcs/requirements/mariadb/tools/user-init.sh
@@ -11,29 +11,29 @@ elif [ -z "$MARIADB_USER" ]; then
 fi
 
 
-if [ ! -f "/etc/mysql/init.sql" ]; then
-  touch /etc/mysql/init.sql
-  echo creaded file init.sql
-fi
 
-chmod 0777 /etc/mysql/init.sql
+mkdir -p /run/mysqld/
+chown -R mysql:mysql /run/mysqld/
+#FILE=run/mysqld/mysqld.sock
+
+#if [ ! -f "$FILE" ]; then
+#  touch /run/mysqld/mysqld.sock
+#  echo created file mysqld.sock
+#fi
+
+#if [ ! -f "/etc/mysql/init.sql" ]; then
+#  touch /etc/mysql/init.sql
+#  echo creaded file init.sql
+#fi
+
 cat << EOF > /tools/init.sql
 CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
 GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
 EOF
+chmod 777 /etc/mysql/init.sql
 
-FILE=run/mysqld/mysqld.sock
 
-if [ ! -f "$FILE" ]; then
-  mkdir -p /run/mysqld/
-  touch /run/mysqld/mysqld.sock
-  echo created file mysqld.sock
-fi
+sed -i "s/bind-address            = 127.0.0.1/bind-address            = mariadb/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 
-sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
-sed -i "s/^user[[:space:]]*=[[:space:]]*mysql/user = root/" "/etc/mysql/mariadb.conf.d/50-server.cnf"
-
-mariadbd
-
-echo AFTER CREATION OF USER AND DATABASE
+mariadbd 
