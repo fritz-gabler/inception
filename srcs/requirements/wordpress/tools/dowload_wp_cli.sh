da75c7789dc30494001dce3e660813791b0c2d79
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-12-04 16:29:35 +0100

    add creation of config user and author in mariadb sh file

--- srcs/requirements/wordpress/tools/dowload_wp_cli.sh
+++ srcs/requirements/wordpress/tools/dowload_wp_cli.sh
@@ -4,11 +4,12 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-MARIADB_PASSWORD = $(cat /run/secrets/maria-pass)
-AUTHOR_PASS = $(cat /run/secrets/author_pass)
-WP_DB_USER_PASS = $(cat /run/secrets/wp-db-user-pass)
+#MARIADB_PASSWORD = $(cat /run/secrets/maria-pass)
+#AUTHOR_PASS = $(cat /run/secrets/author_pass)
+#WP_DB_USER_PASS = $(cat /run/secrets/wp-db-user-pass)
 
 mkdir -p /var/www/html/mysite
+mkdir -p /run/php
 
 if [ ! -f "/var/www/html/mysite/index.php" ]; then
   wp core download --allow-root --path="/var/www/html/mysite/"
@@ -20,12 +21,22 @@ if [ ! -f "/var/www/html/mysite/wp-config.php" ]; then
   wp config create \
   --dbname=$MARIADB_DATABASE \
   --dbuser=$MARIADB_USER \
-  --dbpass=$MARIADB_PASSWORD \
   --dbhost='mariadb' \
   --allow-root \
-  --path="/var/www/html/mysite/"
+  --path="/var/www/html/mysite/" \
+  --prompt=dbpass < /run/secrets/maria-pass \
 
 
+fi
+
+if ! wp core is-installed --path="/var/www/html/mysite/" --allow-root; then
+  wp core install \
+    --url=https://fgabler.42.fr \
+    --title=inception \
+    --admin_user=$WP_DB_USER \
+    --admin_email=$WP_DB_EMAIL \
+    --allow-root \
+    --prompt=admin_password < /run/secrets/wp-db-user-pass
 fi
 
 if ! wp user exists $AUTHOR_NAME --allow-root; then
@@ -33,20 +44,11 @@ if ! wp user exists $AUTHOR_NAME --allow-root; then
     $AUTHOR_NAME \
     $AUTHOR_MAIL \
     --role=author \
-    --user_pass=$AUTHOR_PASS \
-    --allow-root
+    --allow-root \
+    --prompt=user_pass < /run/secrets/author_pass
   echo 'Author user got created'
 fi
 
 
-if ! wp core is-installed --path="/var/www/html/mysite/" --allow-root; then
-  wp core install \
-    --url=https://fgabler.42.fr \
-    --title=inception \
-    --admin_user=$WP_DB_USER \
-    --admin_password=$WP_DB_USER_PASS \
-    --admin_email=$WP_DB_EMAIL \
-    --allow-root
-fi
 
-php-fpm8.2 -F
+php-fpm7.4 -F
